bb8ced65f49aea7b1a9d42d8159e2c94d3e657c9
flortalavera <florii.talavera@gmail.com>
2023-04-09 12:21:43 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { HeaderComponent } from './home/header/header.component';
+import { PresentationComponent } from './home/presentation/presentation.component';
+import { PlansComponent } from './home/plans/plans.component';
+import { FooterComponent } from './home/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    HeaderComponent,
+    PresentationComponent,
+    PlansComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
